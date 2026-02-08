7afe4b57c472a0c6709cb69e2d287106b480462d
Kemeshat <kemeshat@usf.edu>
2026-02-08 16:31:00 -0500

    changes made to customer tracker

--- cc_3.js
+++ cc_3.js
@@ -1,48 +1,32 @@
 // create custom records
 let customers = [
-  {
-    name: "Kevin Bates",
-    email: "kevinb@gmail.com",
-    purchases: ["Airfryer", "Blender"]
-  },
-  {
-    name: "Toby Richards",
-    email: "toby.richards@gmail.com",
-    purchases: ["microwave", "Toaster" ]
-  },
-  {
-    name: "Peter Kinight",
-    email: "peterkinight@gmail.com",
-    purchases: ["Coffee Maker", "Kettle"]
-  }
+  {name: "Toby Richards", email: "toby.richards@gmail.com", purchases: ["microwave", "Toaster"]},
+  {name: "Kevin Reid", email: "kevinb@gmail.com", purchases: ["Airfryer", "Blender"]},
+  {name: "Peter Knight", email: "peterkinight@gmail.com", purchases: ["Coffee Maker", "Kettle"]}
 ];
 console.log (customers);
 
 // Add a new customer to the array
-customers.push({
-  name: "Sarah Johnson",
-  email: "sarah.johnson@gmail.com",
-  purchases: ["washing machine", "dryer"]
-});
+customers.push({name: "Sarah Johnson", email: "sarah.johnson@gmail.com", purchases: ["washing machine", "dryer"]});
 console.log (customers);
+
 
 // Remove the first customer
 customers.shift();
 console.log (customers);
 
 // Update the email of one existing customer
-customers[0].email = "toby.richards.new@gmail.com";
+customers[0].email = "newemail@example.com";
 console.log (customers);
 
 //add a new purchase to one of the existing customers
-customers[0].purchases.push("Air Conditioner");
+customers[2].purchases.push("Air Conditioner");
 console.log (customers);
 
-customers.forEach(customer => {
-  console.log (
-    `name: ${customer.name},
-    email: ${customer.email},
-    purchases: ${customer.purchases.join(", ")}`
-  );
+customers.forEach(customer => { 
+  console.log( `Name: ${customer.name}, Email: ${customer.email}, Purchases: ${customer.purchases.length}` );
 });
+
+
+
 
